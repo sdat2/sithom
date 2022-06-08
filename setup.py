6a295db046c6fe16c14337e9b1cfb37de759b84a
Simon D.A. Thomas <sdat2@cam.ac.uk>
2022-06-08 17:52:34 +0100

    fix: changing test matrix

--- setup.py
+++ setup.py
@@ -89,7 +89,6 @@ setup(
         "Intended Audience :: Science/Research",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
