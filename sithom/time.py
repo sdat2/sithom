f91880331fba1d82d1177e17d71ed658d2b6e643
Simon D.A. Thomas <sdat2@cam.ac.uk>
2022-07-18 16:44:58 +0100

    fix: fixing indentation in ticker

--- sithom/time.py
+++ sithom/time.py
@@ -65,59 +65,59 @@ def time_limit(seconds: int) -> None:
 
 
 class Ticker(threading.Thread):
-  """A very simple thread that merely blocks for :attr:`interval` and sets a
-  :class:`threading.Event` when the :attr:`interval` has elapsed. It then waits
-  for the caller to unset this event before looping again.
+    """A very simple thread that merely blocks for :attr:`interval` and sets a
+    :class:`threading.Event` when the :attr:`interval` has elapsed. It then waits
+    for the caller to unset this event before looping again.
 
-  https://stackoverflow.com/a/8420498
+    https://stackoverflow.com/a/8420498
 
-  Example use::
+    Example use::
 
-    
-    >>> t = Ticker(1.0) # make a ticker
-    >>> t.start() # start the ticker in a new thread
-    >>> try:
-    ...     while t.evt.wait(): # hang out til the time has elapsed
-    ...     t.evt.clear() # tell the ticker to loop again
-    ...     print time.time(), "FIRING!"
-    ... except:
-    ...     t.stop() # tell the thread to stop
-    ...     t.join() # wait til the thread actually dies
+        >>> from sithom.time import Ticker
+        >>> t = Ticker(1.0) # make a ticker
+        >>> t.start() # start the ticker in a new thread
+        >>> try:
+        ...     while t.evt.wait(): # hang out til the time has elapsed
+        ...         t.evt.clear() # tell the ticker to loop again
+        ...         print(time.time(), "FIRING!")
+        ... except:
+        ...     t.stop() # tell the thread to stop
+        ...     t.join() # wait til the thread actually dies
 
-  """
-  # SIGALRM based timing proved to be unreliable on various python installs,
-  # so we use a simple thread that blocks on sleep and sets a threading.Event
-  # when the timer expires, it does this forever.
-  def __init__(self, interval):
-    super(Ticker, self).__init__()
-    self.interval = interval
-    self.evt = threading.Event()
-    self.evt.clear()
-    self.should_run = threading.Event()
-    self.should_run.set()
-
-  def stop(self):
-    """Stop the this thread. You probably want to call :meth:`join` immediately
-    afterwards
     """
-    self.should_run.clear()
+    # SIGALRM based timing proved to be unreliable on various python installs,
+    # so we use a simple thread that blocks on sleep and sets a threading.Event
+    # when the timer expires, it does this forever.
+    def __init__(self, interval):
+        super(Ticker, self).__init__()
+        self.interval = interval
+        self.evt = threading.Event()
+        self.evt.clear()
+        self.should_run = threading.Event()
+        self.should_run.set()
 
-  def consume(self):
-    was_set = self.evt.is_set()
-    if was_set:
-      self.evt.clear()
-    return was_set
+    def stop(self):
+        """Stop the this thread. You probably want to call :meth:`join` immediately
+        afterwards
+        """
+        self.should_run.clear()
 
-  def run(self):
-    """The internal main method of this thread. Block for :attr:`interval`
-    seconds before setting :attr:`Ticker.evt`
+    def consume(self):
+        was_set = self.evt.is_set()
+        if was_set:
+            self.evt.clear()
+        return was_set
 
-    .. warning::
-      Do not call this directly!  Instead call :meth:`start`.
-    """
-    while self.should_run.is_set():
-      time.sleep(self.interval)
-      self.evt.set()
+    def run(self):
+        """The internal main method of this thread. Block for :attr:`interval`
+        seconds before setting :attr:`Ticker.evt`
+
+        .. warning::
+        Do not call this directly!  Instead call :meth:`start`.
+        """
+        while self.should_run.is_set():
+            time.sleep(self.interval)
+            self.evt.set()
 
 
 def time_stamp() -> str:
