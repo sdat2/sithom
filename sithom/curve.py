01c17114e173af643ce3982a66d51155f3112544
Simon D.A. Thomas <sdat2@cam.ac.uk>
2025-02-03 11:22:12 +0000

    exp with fit

--- sithom/curve.py
+++ sithom/curve.py
@@ -8,7 +8,7 @@ TODO: Could be improved so that we just have to referenc a polynomial by a boole
 TODO: Could be improved by normalizing the input data, so that the fit is more stable.
 """
 
-from typing import Callable, Tuple, Sequence, Union, Literal
+from typing import Callable, Tuple, Sequence, Union, Literal, List, Dict
 import numpy as np
 from uncertainties import unumpy as unp, ufloat
 from scipy.optimize import curve_fit
@@ -16,28 +16,15 @@ from .unc import tex_uf
 
 Flt = Union[float, ufloat]
 
-
-def _cubic(x: float, a: Flt, b: Flt, c: Flt, d: Flt) -> Flt:
-    """Fit cubic curve to data"""
-    return a * (x**3) + b * (x**2) + c * x + d
-
-
-def _parab(x: float, a: Flt, b: Flt, c: Flt) -> Flt:
-    """Fit parabola to data."""
-    return a * (x**2) + b * x + c
+REG_TYPE_DICT: Dict[str, List[bool]] = {
+                 "lin": [True, True],
+                 "lin_0": [True, False],
+                 "parab": [True, True, True], 
+                 "cubic": [True, True, True, True]}
 
 
-def _lin(x: float, a: Flt, b: Flt) -> Flt:
-    """Fit line to data using curve_fit."""
-    return (a * x) + b
-
-
-def _lin_0(x: float, a: Flt) -> Flt:
-    """Fit line through zero data using curve_fit."""
-    return a * x
-
-
-def _return_func(param: unp.uarray, reg_type: str = "lin") -> Callable:
+def _return_func(param: Union[np.ndarray, unp.uarray], 
+                 reg_type: Union[str, List[bool]] = "lin") -> Callable:
     """
     Return function so that the linear function only has to be referenced once.
 
@@ -51,24 +38,58 @@ def _return_func(param: unp.uarray, reg_type: str = "lin") -> Callable:
     Example of returning the linear function::
         >>> from sithom.curve import _return_func
         >>> func = _return_func(unp.uarray([1, 2], [0.5, 1]), reg_type="lin")
+        >>> func = _return_func(np.ndarray([1, 2]), reg_type="lin")
+        >>> assert np.isclose(func([1]), 3.0)
     """
 
-    def lin(x: Sequence[Flt]) -> np.array:
-        return _lin(np.array(x), param[0], param[1])
+    if isinstance(reg_type, str):
+        assert reg_type in REG_TYPE_DICT
+        reg_type = REG_TYPE_DICT[reg_type]
 
-    def lin_0(x: Sequence[Flt]) -> np.array:
-        return _lin_0(np.array(x), param[0])
+    def _func(x: Sequence[Flt]) -> np.array:
+        x = np.array(x)
+        order = len(reg_type)
+        j = 0
+        output = 0
+        for i, power in enumerate(range(order - 1, -1, -1)):
+            if reg_type[i]:
+                output += param[j] * (np.array(x)**power)
+                j += 1
+        return output
 
-    def parab(x: Sequence[Flt]) -> np.array:
-        return _parab(np.array(x), param[0], param[1], param[2])
+    return _func
 
-    def cubic(x: Sequence[Flt]) -> np.array:
-        return _cubic(np.array(x), param[0], param[1], param[2], param[3])
 
-    func_dict = {"lin": lin, "lin0": lin_0, "parab": parab, "cubic": cubic}
+def _return_func_opt(reg_type: Union[str, List[bool]]) -> Callable:
+    """Return the function with arguments to optimize.
 
-    assert reg_type in func_dict
-    return func_dict[reg_type]
+    Args:
+        reg_type (Union[str, List[bool]]): Which regression to do.
+
+    Returns:
+        Callable[[Sequence[Flt], Flt], Flt]: Function to optimize.
+    """
+
+    if isinstance(reg_type, str):
+        assert reg_type in REG_TYPE_DICT
+        reg_type = REG_TYPE_DICT[reg_type]
+
+    def _func(x: Sequence[Flt], *param: Flt) -> Flt:
+        x = np.array(x)
+        order = len(reg_type)
+        j = 0
+        output = 0
+        for i, power in enumerate(range(order - 1, -1, -1)):
+            if reg_type[i]:
+                output += param[j] * (np.array(x)**power)
+                j += 1
+        return output
+    
+    """
+    def _func_one(x, p0, p1, p2):
+        _func()    """
+
+    return _func
 
 
 def fit(
@@ -96,11 +117,8 @@ def fit(
         >>> assert np.isclose(param[1].n, 1.00)
 
     """
-    func_dict = {"lin": _lin, "lin0": _lin_0, "parab": _parab, "cubic": _cubic}
 
-    assert reg_type in func_dict
-
-    popt, pcov = curve_fit(func_dict[reg_type], x_npa, y_npa)
+    popt, pcov = curve_fit(_return_func_opt(reg_type=reg_type), x_npa, y_npa)
     perr = np.sqrt(np.diag(pcov))
     param = unp.uarray(popt, perr)
 
